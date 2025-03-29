20792d82aa07afc0fc26758c5e423f35148ef8a1
Karla Lucia Lin <165141827+LynxxyLucy@users.noreply.github.com>
2025-03-29 11:40:10 +0000

    initial commit

---
+++ endofday.sh
@@ -0,0 +1,10 @@
+~/.scripts/sysupdate.sh
+
+echo ""
+echo "----------------------------------------------------"
+echo "POWER OFF"
+echo "----------------------------------------------------"
+echo "(in 5 seconds)"
+
+sleep 5
+poweroff
